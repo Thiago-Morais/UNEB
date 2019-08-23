0ac244b863a6dca97b47ef25d7c20cc1f90b883f
Thiago-Morais <thiolimor@gmail.com>
2019-08-23 00:51:10 -0300

    Funcao para adicionar produto novo no carro

--- LP1/Codes/GetProduct/main.c
+++ LP1/Codes/GetProduct/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define debug(a) printf("%i\n", a)
+#define debug(a) printf("check_%d\n", a)
+#define prints(a) printf("%s\n", a);
 /*
 Pedir codigo do produto
 Buscar pelo codigo no produos.txt
@@ -16,14 +17,15 @@ typedef struct prod
     float price;
 }Prod;
 
-typedef struct cart
+typedef struct cartNode
 {
     struct prod *content;
-    struct cart *next;
-}Cart;
+    struct cartNode *next;
+}CartNode;
 
 Prod* GetProdByCod(FILE *fp, unsigned long cod)         //Procura um produto referente a um codigo passado, armazena as informações em
 {
+    prints("GetProdByCod");
     /*
 
         Ler o primeiro undiggned long
@@ -36,16 +38,15 @@ Prod* GetProdByCod(FILE *fp, unsigned long cod)         //Procura um produto ref
             retorna esse prod
     Se terminar e não achar, retorna NULL
     */
-
     Prod *product = (Prod*) malloc(sizeof(Prod));
     unsigned long codComp;
 
     fscanf(fp, "%lu", &codComp);    while(codComp != cod)
-    {
-        while(fgetc(fp) != '\n');
+    {        while(fgetc(fp) != '\n');
         fscanf(fp, "%lu", &codComp);
     }
-    fscanf(fp, ";%[^;];%f\n", &product->name, &product->price);
+
+    fscanf(fp, ";%[^;];%f\n", product->name, &product->price);
     product->cod = codComp;
 
     //DEBUG
@@ -58,11 +59,13 @@ Prod* GetProdByCod(FILE *fp, unsigned long cod)         //Procura um produto ref
     //while(fgetc(fp) != '\n');
 
     //printf("%c\n", fgetc(fp));
+    rewind(fp);
     return product;
 }
 
 Prod* GetProdInLine(FILE *fp)               //Pega as informações de um produto no inicio da linha, armazena num struct produto e retorna o ponteiro pra esser produto
 {
+    prints("GetProdInLine");
     /*
     Cria um produto
     Cria um ponteiro pra esse produto
@@ -70,25 +73,72 @@ Prod* GetProdInLine(FILE *fp)               //Pega as informações de um produto 
     Retorna o ponteiro
     */
     Prod *product = (Prod*) malloc(sizeof(Prod));
-    fscanf(fp, "%lu;%[^;];%f\n", &product->cod, &product->name, &product->price);
+    fscanf(fp, "%lu;%[^;];%f\n", &product->cod, product->name, &product->price);
     printf("1 == %lu \n%s \n%.2f\n", product->cod, product->name, product->price);
     return product;
 }
 
 void PrintProduct(Prod *product)
 {
+    prints("PrintProduct");
     printf("\nProduct Code = %lu\n", product->cod);
     printf("Product Name = %s \n", product->name);
     printf("Product Price = %.2f\n", product->price);
 }
 
+void AddProdInCart(Prod **prod, CartNode **cart)
+{
+    prints("AddProdInCart");
+    /*
+    Verificar se cart está vazio
+    Senão
+        Ir ate o final
+    Inserir no final
+    */
+    CartNode *newContent = (CartNode*)malloc(sizeof(CartNode));
+    newContent->next = NULL;
+    newContent->content = prod;
+
+    if(!*cart) *cart = newContent;
+    else
+    {
+        CartNode *currentNode = *cart;
+
+        while(currentNode->next != NULL) currentNode = currentNode->next;
+
+        currentNode->next = newContent;
+    }
+}
+
+void PrintCart(CartNode **cart)
+{
+    prints("PrintCart");
+
+    if (!cart) return;
+
+    CartNode *curNode = *cart;
+
+    while(curNode != NULL)
+    {
+        PrintProduct(curNode->content);
+        curNode = curNode->next;
+    }
+
+}
+
 int main()
 {
     FILE *fp = fopen("produtos.txt", "r");
-    Prod *cart = NULL;
+    CartNode *cart = NULL;
 
     //GetProdInLine(fp);
-    PrintProduct(GetProdByCod(fp,123654));
+    //PrintProduct(GetProdByCod(fp,123654));
+
+    AddProdInCart(GetProdByCod(fp, 123654),&cart);
+    AddProdInCart(GetProdByCod(fp, 456123),&cart);
+    AddProdInCart(GetProdByCod(fp, 456123),&cart);
+    AddProdInCart(GetProdByCod(fp, 456123),&cart);
+    PrintCart(&cart);
 
     //printf("%c\n", fgetc(fp));
 
